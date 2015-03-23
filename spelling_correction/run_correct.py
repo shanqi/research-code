dd4d186bc521a5050d8435557c002aa3be0a7aee
Qi Shan <qis@zillow.com>
2015-03-23 11:24:56 -0700

    spelling correction

---
+++ spelling_correction/run_correct.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import sys
+from correct_v1 import correct
+from tokenize import generate_tokens
+
+def is_number(s):
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
+
+fname = sys.argv[1]
+outfilename = fname + '.corrected.txt'
+print fname + ' -> ' + outfilename
+inputfile = open(fname,'r')
+outputfile = open(outfilename,'w')
+line_count = 0
+for line in inputfile:
+  g = line.split(',')
+  outputfile.write(g[0]+',')
+  gsplit = g[1].split(' ')
+  for entry in gsplit:
+    entryClean = entry.replace('\n','').lower()
+    if (is_number(entryClean)):
+      outputfile.write(entryClean+' ')
+    else:
+      correctedWord = correct(entryClean)
+      outputfile.write(correctedWord+' ')
+  outputfile.seek(-1,2)
+  outputfile.write('\n')
+  line_count += 1
+  if ( line_count%100 == 0 ):
+    print 'line_count: ' + str(line_count)
+
+inputfile.close()
+outputfile.close()
+
+
